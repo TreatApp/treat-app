d838640791dc2ba7b1a30c64c87c89436ba8400f
Victor Stodell <victor@stodell.se>
2016-03-19 02:28:56 +0100

    Event detail view

--- app/components/event/index.jsx
+++ app/components/event/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { getBlobUrl, getImageUrl, formatDate } from '../../utils/helpers';
+import Rating from '../rating';
 
 class Event extends Component {
 
@@ -20,18 +22,53 @@ class Event extends Component {
       let { eventsState, userEventsState } = this.props;
       let { events } = eventsState.merge(userEventsState).toJS();
 
-      let event = events.find(e => {
-         return e.eventId = eventId;
-      });
+      let event = events.find(e => { return e.id == eventId; });
 
-      if(!event) {
-         return <div>Event not found</div>;
-      }
+      let { user, slotsAvailable, slots, location, title, categories, start, end, description, price, eventImages } = event;
+      let { externalId, rating, firstName, lastName } = user;
 
-      let { title } = event;
+      let imageUrl = getImageUrl(externalId, 'square');
+      let backgroundUrl = 'url(' + getBlobUrl(eventImages) + ')';
+
       return (
-         <div>
-            <h1>{title}</h1>
+         <div className="text-center">
+            <div style={{background: backgroundUrl, backgroundSize: '100%', height: 100}}></div>
+            <h3>{title}</h3>
+            <p>
+               {categories.map(category => {
+                  return <em key={category.id}>{category.name}</em>;
+               })}
+            </p>
+            <hr />
+            <div className="row">
+               <div className="col-xs-4">
+                  <i className="fa fa-2x fa-clock-o"></i><br />
+                  <strong>{formatDate(start, 'll')}</strong><br />
+                  {formatDate(start, 'LT')} - {formatDate(end, 'LT')}
+               </div>
+               <div className="col-xs-4">
+                  <i className="fa fa-2x fa-user"></i><br />
+                  <strong>{slots} seats</strong><br />
+                  {slotsAvailable} available
+               </div>
+               <div className="col-xs-4">
+                  <i className="fa fa-2x fa-money"></i><br />
+                  {price} SEK<br />
+                  (VAT incl.)
+               </div>
+            </div>
+            <hr />
+            <div>
+               <img src={imageUrl} className="img-circle" /><br />
+               Hosted by<br />
+               <a href="/user/profile/{{user.id}}">{firstName} {lastName}</a><br />
+               <Rating rating={rating || 0} />
+            </div>
+            <hr />
+            <p className="text-left">{description}</p>
+            <hr />
+            <br /><br />
+            <button className="btn btn-primary btn-block">Send request</button>
          </div>
       );
    }
